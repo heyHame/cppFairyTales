72dc6e567b893374133a29add2fefd84800809ef
Hame Tex <58879098+heyHame@users.noreply.github.com>
2019-12-28 12:28:26 +0800

    Update littleRedRidingHood.cpp

--- littleRedRidingHood.cpp
+++ littleRedRidingHood.cpp
@@ -3,5 +3,13 @@
 int main() {
  once_upon_a time;
  girl littleredridinghood;
+ vector<people> villagers;
+ people *ALL_OF_THE_VILLAGERS = villagers;
  littleredridinghood.little = true;
- 
+ place A_SMALL_VILLAGE;
+ A_SMALL_VILLAGE.isNear(THE_FOREST);
+ littleredridinghood.livedAt(A_SMALL_VILLAGE);
+ do {
+  littleredridinghood.wears(RED_RIDING_CLOAK);
+ } while (littleredridinghood.isOut());
+ ALL_OF_THE_VILLAGERS.calls(littleredridinghood, "Little red Riding Hood");
